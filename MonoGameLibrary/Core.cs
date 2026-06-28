e21041656d44b543d8cf47a40473d16d7f009f96
Sebastian Haba <sebastian@haba.net.pl>
2026-06-28 11:24:42 +0200

    Inicjlizacja repo. Tutoril z monogame.

---
+++ MonoGameLibrary/Core.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using MonoGameLibrary.Input;
+
+namespace MonoGameLibrary;
+
+public class Core : Game
+{
+    internal static Core _instance;
+
+    
+    public Core(string title, int width, int height, bool fullScreen)
+    {
+        // Ensure that multiple cores are not created.
+        if (_instance != null)
+        {
+            throw new InvalidOperationException($"Only a single Core instance can be created");
+        }
+
+        // Store reference to engine for global member access.
+        _instance = this;
+
+        // Create a new graphics device manager.
+        Graphics = new GraphicsDeviceManager(this);
+
+        // Set the graphics defaults.
+        Graphics.PreferredBackBufferWidth = width;
+        Graphics.PreferredBackBufferHeight = height;
+        Graphics.IsFullScreen = fullScreen;
+
+        // Apply the graphic presentation changes.
+        Graphics.ApplyChanges();
+
+        // Set the window title.
+        Window.Title = title;
+
+        // Set the core's content manager to a reference of the base Game's
+        // content manager.
+        Content = base.Content;
+
+        // Set the root directory for content.
+        Content.RootDirectory = "Content";
+
+        // Mouse is visible by default.
+        IsMouseVisible = true;
+        
+        // Exit on escape is true by default
+        ExitOnEscape = true;
+    }
+
+    protected override void Initialize()
+    {
+        base.Initialize();
+
+        // Set the core's graphics device to a reference of the base Game's
+        // graphics device.
+        GraphicsDevice = base.GraphicsDevice;
+
+        // Create the sprite batch instance.
+        SpriteBatch = new SpriteBatch(GraphicsDevice);
+        
+        // Create a new input manager.
+        Input = new InputManager();
+    }
+    
+    
+    protected override void Update(GameTime gameTime)
+    {
+        // Update the input manager.
+        Input.Update(gameTime);
+
+        if (ExitOnEscape && Input.Keyboard.WasKeyJustPressed(Keys.Escape))
+        {
+            Exit();
+        }
+
+        base.Update(gameTime);
+    }
+    
+    public static Core Instance => _instance;
+    
+    public static GraphicsDeviceManager Graphics { get; private set; }
+    
+    public new static GraphicsDevice GraphicsDevice
+    {
+        get;
+        private set;
+    }
+    
+    public static SpriteBatch SpriteBatch { get; private set; }
+    
+    public new static ContentManager Content { get; private set; }
+    
+    /// <summary>
+    /// Gets a reference to the input management system.
+    /// </summary>
+    public static InputManager Input { get; private set; }
+
+    /// <summary>
+    /// Gets or Sets a value that indicates if the game should exit when the esc key on the keyboard is pressed.
+    /// </summary>
+    public static bool ExitOnEscape { get; set; }
+}
